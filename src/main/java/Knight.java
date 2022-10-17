f82eca23c57473add1ce66f18223afb638e37e89
Sherkhan <kur.sherkhan2000@gmail.com>
2022-10-17 22:15:46 +0600

    Initial commit

---
+++ src/main/java/Knight.java
@@ -0,0 +1,8 @@
+public class Knigth extends Piece {
+
+    private static final String NAME = "Knigth";
+
+    public Knigth(Spot spot, String id, boolean white) {
+        super(NAME, spot, id, white);
+    }
+}
---
+++ src/main/java/Rook.java
@@ -0,0 +1,8 @@
+public class Roor extends Piece{
+
+    private static final String NAME = "Roor";
+
+    public Roor(Spot spot, String id, boolean white) {
+        super(NAME, spot, id, white);
+    }
+}
